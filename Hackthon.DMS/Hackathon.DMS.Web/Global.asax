198d95a5557b9434fa07079ae07451a776be9066
pankajw-cuelogic <pankaj.wakchaure@cuelogic.co.in>
2016-03-05 12:16:55 +0530

    master : first push environment setup

---
+++ Hackthon.DMS/Hackathon.DMS.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hackathon.DMS.Web.MvcApplication" Language="C#" %>
---
+++ Hackthon.DMS/Hackathon.DMS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hackathon.DMS.Web.Startup))]
+namespace Hackathon.DMS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
